811965804bc7a1d0f38418b29e93c4d6c9ecdf81
mihailo2001 <joc.mihailo@gmail.com>
2024-07-01 20:21:54 +0200

    Connest frontend and backend using axios.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [listOfPosts, setListOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts")
+    .then((response) => {
+      setListOfPosts(response.data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }, []);
+
   return (
     <div className="App">
-    
+      { listOfPosts.map((value, key) => {
+        return <div className='post'> 
+          <div className='title'> {value.title} </div> 
+          <div className='body'> {value.postText} </div>
+          <div className='footer'> {value.username} </div>
+        </div>
+      }) }
     </div>
   );
 }
