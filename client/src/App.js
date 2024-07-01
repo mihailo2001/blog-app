946a68d2829595a8f89f6e0b17f25b6f9fa4d8f4
mihailo2001 <joc.mihailo@gmail.com>
2024-07-01 20:53:29 +0200

    Add CreatePage Component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
 
 function App() {  
 
   return (
     <div className="App">
       <Router>
+        <Link to="/createpost"> Create A Post </Link>
+        <Link to="/"> Homepage </Link>
         <Routes>
           <Route path="/" exact element={<Home/>}/>
-          
+          <Route path="/createpost" exact element={<CreatePost/>}/>
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessages } from 'formik';
+
+const CreatePost = () => {
+  return (
+    <div className='createPostPage'>
+        <Formik>
+            <Form>
+                <label>Title: </label>
+                <Field 
+                    id="inputCreatePost" 
+                    name="title" 
+                    placeholder="(Ex. Title...)"
+                />
+                <label>Post: </label>
+                <Field 
+                    id="inputCreatePost" 
+                    name="postText" 
+                    placeholder="(Ex. Post...)"
+                />
+                <label>Username: </label>
+                <Field 
+                    id="inputCreatePost" 
+                    name="username" 
+                    placeholder="(Ex. John123...)"
+                />
+                <button type='submit'> Create Post </button>
+            </Form>
+        </Formik>
+    </div>
+  )
+}
+
+export default CreatePost
